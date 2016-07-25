463bc6996b5f9f4d8cf5fed79bc3b25bc31fd352
Zach Azar <zach.azar@gmail.com>
2016-07-25 09:40:21 -0700

    Started building out About. Added styling. Added photo.

--- app/components/about/about.ctrl.js
+++ app/components/about/about.ctrl.js
@@ -1,1 +1,10 @@
-export default class AboutCtrl{}
+//About photo
+import photo_zach from 'assets/ZachAzar_Professional_small.jpg';
+
+import './about.scss';
+
+export default class AboutCtrl{
+  constructor() {
+    this.photo_zach = photo_zach;
+  }
+}
