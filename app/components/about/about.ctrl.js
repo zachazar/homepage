0e99f17d3d50d4a7c222ea52fa0a5159aa094035
Zach Azar <zach.azar@gmail.com>
2016-07-26 18:21:03 -0700

    Added Contact page. Needs functionality

--- app/components/about/about.ctrl.js
+++ app/components/about/about.ctrl.js
@@ -1,5 +1,5 @@
 //About photo
-import photo_zach from 'assets/ZachAzar_Professional_small.jpg';
+import photo_zach from 'assets/images/ZachAzar_Professional_small.jpg';
 
 import './about.scss';
 
--- app/components/contact/contact.ctrl.js
+++ app/components/contact/contact.ctrl.js
@@ -1,1 +1,8 @@
-export default class ContactCtrl{}
+import './contact.scss';
+import photo_skate from 'assets/images/ZachAzar_skate.jpg';
+
+export default class ContactCtrl{
+  constructor(){
+    this.photo = photo_skate;
+  }
+}
