5f89d238fb74f9b890f72eb9ce8ff05193b4bf93
Zach Azar <zach.azar@gmail.com>
2016-07-28 10:44:41 -0700

    Changed About photo and made splash responsive

--- app/components/about/about.ctrl.js
+++ app/components/about/about.ctrl.js
@@ -1,5 +1,5 @@
 //About photo
-import photo_zach from 'assets/images/ZachAzar_Professional_small.jpg';
+import photo_zach from 'assets/images/ZachAzar_Professional2_600.jpg';
 
 import './about.scss';
 
