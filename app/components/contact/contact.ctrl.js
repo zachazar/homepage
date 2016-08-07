1a2df728ea45b4fc19137134e7c98edf4badf95c
Zach Azar <zach.azar@gmail.com>
2016-08-07 14:22:31 -0700

    Added Spam checking to frontend. Added additional status messages for user including a sending message when waiting on backend.

--- app/components/contact/contact.ctrl.js
+++ app/components/contact/contact.ctrl.js
@@ -1,5 +1,6 @@
 import './contact.scss';
 import photo_skate from 'assets/images/ZachAzar_skate.jpg';
+import spamWordList from 'raw!../../../api/lib/SpamWordList.txt';
 
 export default class ContactCtrl{
   constructor($http){
@@ -20,19 +21,34 @@ export default class ContactCtrl{
         statusClass: "good",
         statusMessage: "Message sent. Thanks!"
       },
-      invalid:  {
+      invalid: {
         statusClass: "bad",
         statusMessage: "Form invalid. Please check the fields above."
+      },
+      empty: {
+        statusClass: "bad",
+        statusMessage: "Form invalid. No fields can be empty."
+      },
+      spam: {
+        statusClass: "bad",
+        statusMessage: "Spam words detected. Please check message for words that might appear often in spam messages."
       },
       error: {
         statusClass: "bad",
         statusMessage: "An error occurred. I'm sorry for the inconvenience. Please feel free to reach out to me on LinkedIn (profile link on bottom of page)"
+      },
+      sending: {
+        statusClass: "processing",
+        statusMessage: "Sending..."
       }
     };
 
     //Status variables that the form binds to
     this.statusMessage="";
     this.statusClass="";
+
+    //Spam word list to help detect spam bots
+    this.spamWordList = spamWordList.split('\n');
   }
 
   SubmitForm(){
@@ -42,7 +58,7 @@ export default class ContactCtrl{
     //If honeyPot is filled, don't bother validating. It's a bot
     if (this.form.honeyPot !== ""){
       this.setStatus("invalid");
-      return false;
+      return;
     }
 
     //Copy form fields and validate
@@ -59,13 +75,16 @@ export default class ContactCtrl{
       return;
     }
     
-    if (!this.validate(toValidate))
+    let validateTuple = this.validate(toValidate);
+
+    if (!validateTuple.success)
     {
       //Invalid
-      this.setStatus("invalid");
+      this.setStatus(validateTuple.status);
       return;
     }
     //Send message
+    this.setStatus("sending");
     this.sendMessage(toValidate);
   }
 
@@ -82,11 +101,28 @@ export default class ContactCtrl{
   validate(message){
     //Check for empty
     if (message.name === "" || message.email === "" || message.message === ""){
-      return false;
+      return { success: false, status: "empty"};
+    }
+    //Check for spam words
+    else if (this.containsSpamWords(message.message, this.spamWordList)){
+      return { success: false, status: "spam"};
     }
     else{
-      return true;
+      return { success: true};
     }
+  }
+
+  containsSpamWords(msg,spamWordList){
+    //Convert message and words to lowercase for string matching
+    let msgLower = msg.toLowerCase();
+    for(let i = 0; i < spamWordList.length; ++i){
+      if (msgLower.includes(spamWordList[i].toLowerCase())){
+        //Contains the spam word. Return true
+        return true;
+      }
+    }
+    //Ran out of words to check. Return false
+    return false;
   }
 
   sendMessage(message){
